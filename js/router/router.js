b8966bb64a01d13bdc52554371954d5bf348f57b
Maciej Sitko <maciejsitko@Maciejs-MacBook-Air.local>
2015-09-20 20:01:55 +0100

    Struggle

--- js/router/router.js
+++ js/router/router.js
@@ -1,9 +1,14 @@
 
 var routes = [api,contact,home,learning,user];
 
-var router = new Router(routes),
-    routeChange = bind(router.route, router);
+var b_d = b_d();
 
-router.route();
+var and = b_d.router(routes);
+
+
+// var router = b_d(routes).route();
+var routeChange = bind(b_d.router(routes).route, b_d.router(routes));
+
+// router.route();
 
 window.addEventListener('hashchange', routeChange, false);
